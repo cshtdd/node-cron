3f0318907ee9ee023bb8f89f550a8bb23656f59b
camilin87 <camilin87@gmail.com>
2016-07-03 21:33:48 -0400

    possibly configure log rotation on an hourly basis

--- templates/setupCron.sh
+++ templates/setupCron.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+echo "Starting node-cron"
+
+cp /etc/cron.daily/logrotate /etc/cron.hourly/logrotate
+
 env                                           >> /tmp/.env
 cat /tmp/.env                                 >> /etc/cron.d/my-cron-job
 echo -n "$TASK_SCHEDULE" | cat - /tmp/crontab >> /etc/cron.d/my-cron-job
+
+echo "Running cron"
+
 cron && tail -f /var/log/cron.log
